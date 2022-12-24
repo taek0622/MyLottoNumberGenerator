9b4cfd54f57d992ac63640bbc8864804b7e6f91e
taek0622 <min4644@gmail.com>
2022-12-24 17:53:44 +0900

    Feat:sparkles:: Add Project

---
+++ LottoNumberGenerator/LottoNumberGeneratorApp.swift
@@ -0,0 +1,17 @@
+//
+//  LottoNumberGeneratorApp.swift
+//  LottoNumberGenerator
+//
+//  Created by 김민택 on 2022/12/24.
+//
+
+import SwiftUI
+
+@main
+struct LottoNumberGeneratorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
